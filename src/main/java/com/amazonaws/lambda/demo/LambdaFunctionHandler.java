bf7b546e0313749c4b19be06ca58c6d2f71272aa
Thiago <th.thiago.mendes@gmail.com>
2020-12-02 17:52:39 -0300

    s3 storage

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -2,15 +2,24 @@ package com.amazonaws.lambda.demo;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;1
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Input: " + input);
-
-        // TODO: implement your handler
-        return "Hello from Lambda!";
+        String key_name = "teste";
+        String bucket_name = "data-lake-teste-1";
+        System.out.format("Uploading %s to S3 bucket %s...\n", input, bucket_name);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        try {
+          s3.putObject(bucket_name, key_name, new File(file_path));
+        } catch (AmazonServiceException e) {
+        System.err.println(e.getErrorMessage());
+        System.exit(1);
     }
 
 }
