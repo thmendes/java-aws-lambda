3119ae2f7411e471d39d7218f1347d19d3cd71cf
Thiago Mendes da Silva <thiago.mendes@compasso.com.br>
2020-12-03 13:47:55 -0300

    .

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -3,23 +3,28 @@ package com.amazonaws.lambda.demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;1
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import java.io.File;
+
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Input: " + input);
-        String key_name = "teste";
-        String bucket_name = "data-lake-teste-1";
-        System.out.format("Uploading %s to S3 bucket %s...\n", input, bucket_name);
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
         try {
-          s3.putObject(bucket_name, key_name, new File(file_path));
-        } catch (AmazonServiceException e) {
-        System.err.println(e.getErrorMessage());
-        System.exit(1);
+        	String key_name = "teste";
+        	String bucket_name = "data-lake-teste-1";
+        	System.out.format("Uploading %s to S3 bucket %s...\n", input, bucket_name);
+        	final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion("sa-east-1").build();
+        	File file = new File("teste");
+        	s3.putObject(bucket_name, key_name, file);
+        } 
+        catch (AmazonServiceException e) {
+        	System.err.println(e.getErrorMessage());
+        	System.exit(1);
+    	}
+		return null;
     }
-
 }
